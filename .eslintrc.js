87cd1e932196dc03d88861ab083e7c8cddf5bd88
Seoyoung Jeong <issyo1022@gmail.com>
2022-02-01 04:29:47 +0900

    chore: 절대 경로 세팅, styled-components 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
