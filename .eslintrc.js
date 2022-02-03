c07015bb4ebf943bd9a7d7bcfdc0fc4df8a6dbef
Seoyoung Jeong <issyo1022@gmail.com>
2022-02-03 22:56:16 +0900

    chore: eslintrc rule 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,10 @@ module.exports = {
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'react/prop-types': 0,
+    'import/prefer-default-export': 0,
+    'react/no-unused-prop-types': 0, // 임시
     'react/jsx-filename-extension': [
       2,
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
